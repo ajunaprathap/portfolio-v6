b920d744308595a66345eb5bd1e99902f82b5020
Ajuna Prathap <ajunaprathap@gmail.com>
2020-03-11 11:30:11 -0400

    Portfolio

--- app.js
+++ app.js
@@ -2,26 +2,27 @@ const navSlide = () => {
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-links');
 const navLinks = document.querySelectorAll('.nav-links li');
+const container = document.querySelectorAll('.container') [0];
 
+const toggleClasses = () => {
+  //Burger animation
+  burger.classList.toggle('toggle');
+  container.classList.toggle('no-scroll');
 
-
-  burger.addEventListener('click', () => {
-    //Toggle Nav
-    nav.classList.toggle('nav-active');
-
-    //Animate Links
-    navLinks.forEach((link, index) => {
-
-      if(link.style.animation){
-        link.style.animation = '';
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
-      }
-    });
-    //Burger animation
-    burger.classList.toggle('toggle');
-  });
-
+  //Toggle Nav
+  nav.classList.toggle('nav-active');
 }
 
+  burger.addEventListener('click', () => {
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      link.addEventListener('click', () => {
+        if (nav.classList.contains('nav-active')) {
+          toggleClasses ();
+        }
+      });
+    });
+  toggleClasses();
+ });
+}
 navSlide();
